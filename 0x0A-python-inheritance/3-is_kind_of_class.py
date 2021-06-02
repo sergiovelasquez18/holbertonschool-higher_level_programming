896873799d679aace86de44718dee8fbf66006c4
sergiovelasquez18 <2458@holbertonschool.com>
2021-06-01 20:00:58 -0500

    is kind of class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module define the is_same_class funtion"""
+
+
+def is_kind_of_class(obj, a_class):
+    """returns True or False based on inheritance"""
+    return isinstance(obj, a_class)
