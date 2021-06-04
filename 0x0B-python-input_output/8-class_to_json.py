205841f88b91b1d6d81c310a96930e60f66a2edf
sergiovelasquez18 <2458@holbertonschool.com>
2021-06-03 21:03:10 -0500

    class to jason

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""This module create a function that returns the dict for JSON serialization"""
+"""
+This module create a function that returns the dict for JSON serialization
+"""
 
 
 def class_to_json(obj):
