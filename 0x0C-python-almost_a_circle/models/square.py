6ef47bcf26b4a48ff8adce8a683ade9a42d78467
sergiovelasquez18 <2458@holbertonschool.com>
2021-06-08 20:41:31 -0500

    square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,3 +17,18 @@ class Square(Rectangle):
         """Return the informal representation of Square class"""
         return "[Square] ({:d}) {:d}/{:d} - {:d}".format(
             self.id, self.x, self.y, self.size)
+
+    @property
+    def size(self):
+        """getter of size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """setter of size"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
