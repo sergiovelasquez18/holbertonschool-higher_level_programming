34d7d879f110ebf28a8e5bf19fbb33b86fdd1274
sergiovelasquez18 <2458@holbertonschool.com>
2021-07-13 17:22:52 -0500

    create read user

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,5 @@
+-- Project MySQL
+-- creates the database hbtn_0c_0
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
