7b1bda9cbf0863f464a246394d943c6cb86152da
sergiovelasquez18 <2458@holbertonschool.com>
2021-07-13 23:58:14 -0500

    shows by genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -6,4 +6,4 @@ LEFT JOIN tv_show_genres
 ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title ASC, tv_genres.name ;
+ORDER BY tv_shows.title ASC, tv_genres.nam ;
