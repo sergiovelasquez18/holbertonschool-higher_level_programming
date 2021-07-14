c392afaf1e1242c44cb028074566a6e30911ff31
sergiovelasquez18 <2458@holbertonschool.com>
2021-07-13 23:04:59 -0500

    10

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+-- Project MySQL
+-- lists all shows contained in hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+RIGHT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC
