849284944039f0c983e2dd566642f12b015e8b99
sergiovelasquez18 <2458@holbertonschool.com>
2021-07-13 22:22:10 -0500

    cities

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Project MySQL
+-- reates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities(
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+state_id INT NULL,
+name VARCHAR(256) NOT NULL,
+FOREIGN KEY(state_id) REFERENCES state(id)
+)
