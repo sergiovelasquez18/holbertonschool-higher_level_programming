f37d8a01a40c99f81db180f1dc693459e1e4eefe
sergiovelasquez18 <2458@holbertonschool.com>
2021-07-14 23:24:09 -0500

    change class

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,3 @@
 -- Project MySQL
 -- removes all records with a score <= 5 in the table second_table of the database hbtn_0c_0
-DELETE second_table SET score=10 WHERE name="Bob";
+DELETE second_table WHERE score <= 5;
