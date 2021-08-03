49e27e7cd87c3214a5753737c1a05c4faa860c0d
sergiovelasquez18 <2458@holbertonschool.com>
2021-08-02 22:39:03 -0500

    script that prints the first argument passed

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// prints a message depending of the number of arguments passed
+// script that prints the first argument passed
 if (process.argv[2]) {
   console.log(process.argv[2]);
 } else {
