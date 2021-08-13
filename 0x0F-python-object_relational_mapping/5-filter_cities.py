9c2586bf20426a1756676ab0e872f9a3ca34a6e8
siovelasquez18 <sergioluisvelasquezcarval@gmail.com>
2021-08-13 00:35:07 +0000

    filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -2,19 +2,28 @@
 """script that takes in the name of a state as
 an argument and lists all cities of that state
 """
+
+
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                              passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
-    current = connect.cursor()
-    query = "SELECT cities.name FROM cities"
-    query = query + " INNER JOIN states ON cities.state_id = states.id"
-    query = query + " WHERE states.name = %s ORDER BY cities.id ACS"
-    current.execute(query,[sys.argv[4]])
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    state_to_search = argv[4]
 
-    query_r = current.fetchall()
-    print(', '.join(row[0] for row in query_r))
-    current.close()
-    connect.close()
+    db = MySQLdb.connect(host="localhost", port=3306,
+                         user=mysql_username, passwd=mysql_password,
+                         db=database_name, charset="utf8")
+    cur = db.cursor()
+    statement = """SELECT cities.id, cities.name
+                FROM cities, states
+                WHERE BINARY states.name = %s
+                AND cities.state_id = states.id
+                ORDER BY cities.id ACS"""
+cur.execute(statement, (state_to_search))
+all_rows = cur.fetchall()
+print(", ".join(row[0] for row in all_rows))
+cur.close()
+db.close()
