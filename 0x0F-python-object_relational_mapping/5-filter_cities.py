f56f452c8a9872969e51ddb2ea2986175cc6dfd6
siovelasquez18 <sergioluisvelasquezcarval@gmail.com>
2021-08-13 00:15:58 +0000

    filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -3,22 +3,18 @@
 an argument and lists all cities of that state
 """
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    connect = MySQLdb.connect(host="localhost",
-                              port=3306,
-                              user=argv[1],
-                              passwd=argv[2],
-                              db=argv[3])
+    connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                              passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     current = connect.cursor()
-    current.execute("""SELECT cities.name FROM cities, states WHERE
-    cities.state_id = states.id AND states.name = %s""", (argv[4]))
+    query = "SELECT cities.name FROM cities"
+    query = query + " INNER JOIN states ON cities.state_id = states.id"
+    query = query + " WHERE states.name = %s ORDER BY cities.id ACS"
+    current.execute(query,[sys.argv[4]])
 
     query_r = current.fetchall()
-    new_list = []
-    for row in query_r:
-        new_list.append(row[0])
-    print(', '.join(city[0] for city in query_r))
+    print(', '.join(row[0] for row in query_r))
     current.close()
     connect.close()
