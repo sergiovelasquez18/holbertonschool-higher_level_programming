995bf691cf5cbea1f5f4eeacdaf15c652c932511
sergiovelasquez18 <2458@holbertonschool.com>
2021-09-14 17:37:41 -0500

    script that writes a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-//script that writes a string to a file
+// script that writes a string to a file
 const fs = require('fs');
-fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err,) {
-  if (err) {
-    console.log(err);
-  }
-});
+fs.writeFile(process.argv[2], process.argv[3], 'utf8',
+  function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
