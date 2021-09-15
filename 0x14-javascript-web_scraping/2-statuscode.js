6c536c2a4d0d68e0845db88a2cc90e8bc81295d4
sergiovelasquez18 <2458@holbertonschool.com>
2021-09-14 21:13:28 -0500

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,8 +3,8 @@
 const request = require('request');
 request(process.argv[2], function (error, response) {
   if (error) {
-    console.error('error:', error);
+    console.error('error:', error); // Print the error if one occurred
   } else {
-    console.log('code:', response && response.statusCode);
+    console.log('code:', response && response.statusCode); // Print the response status code if a response was received
   }
 });
