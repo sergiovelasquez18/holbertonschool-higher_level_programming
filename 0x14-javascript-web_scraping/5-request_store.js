e85db5299a97b0d3698102ff297f623757b2961d
sergiovelasquez18 <2458@holbertonschool.com>
2021-09-15 17:38:14 -0500

    request

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file
+const request = require('request');
+const fs = require('fs');
+
+request.get(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  fs.writeFile(process.argv[3], body, (error) => {
+    if (error) {
+    console.log('code:', response && response.statusCode);
+    }
+  });
+});
