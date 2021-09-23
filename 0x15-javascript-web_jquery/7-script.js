be5d47c8d5f029a37baead49cbbaa5b0a0794850
sergiovelasquez18 <2458@holbertonschool.com>
2021-09-22 22:08:11 -0500

    Star wars character

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,8 @@
+const $ = window.$;
+$.ajax({
+  type: 'GET',
+  url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+  success: function (response) {
+    $('DIV#character').text(response.name);
+  }
+});
